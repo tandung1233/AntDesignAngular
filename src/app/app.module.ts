29594b56ab6b0824815f6f8219a40f1b82545b36
TanDung <nguyentandung9111@gmail.com>
2023-03-20 02:48:22 +0700

    đẩy lên git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableBasicComponent } from './table-basic/table-basic.component';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { TableRowSelectionAndOperationComponent } from './table-row-selection-and-operation/table-row-selection-and-operation.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { TableRowSelectionCustomComponent } from './table-row-selection-custom/table-row-selection-custom.component';
+
+
+
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TableBasicComponent, TableRowSelectionAndOperationComponent, TableRowSelectionCustomComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzTableModule,
+    NzDividerModule,
+    NzButtonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
